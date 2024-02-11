7a35578eeef76e459d3a4710577940ef4a589a8d
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-02-11 14:16:46 +0530

    This is the file

--- daily.sh
+++ daily.sh
@@ -3,7 +3,19 @@
 
 
 
-touch file.txt 
+#!/bin/bash
+
+# Get the current date and time
+current_date=$(date +"%Y-%m-%d")
+
+# Output the current date
+echo "Current date: $current_date"
+
+
+
+
+
+touch "$current_date" 
 git add .
 git commit -m  "This is the file"
 git push -u origin master
