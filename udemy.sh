a0ac4da5b64306a52b949138e5ed3dee15bd29e3
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-04-09 22:20:15 +0530

    This is the last message for this project

---
+++ udemy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Deploying the DevOps Configuration..."
+
+# Open Google Chrome with the provided URL
+open -a "Google Chrome" "https://www.udemy.com/course/decodingdevops/?couponCode=KEEPLEARNING"
+
+# Open a new Terminal window
+open -a Terminal
+
+# Change directory to /Users/pradeepsahu/Desktop/DevOps in the new Terminal window
+cd /Users/pradeepsahu/Desktop/DevOps
